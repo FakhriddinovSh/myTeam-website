48a2718fed59d6a3b4cc6962f744738609e5a3e6
FakhriddinovSh <shaxobiddinfaxriddinov@yandex.com>
2022-07-06 18:55:10 +0500

    sdas

---
+++ js/main.js
@@ -0,0 +1,4 @@
+let elBtn = document.querySelectorAll(".directors__icon");
+let elFront = document.querySelectorAll(".directors__content__box")
+let elBack = document.querySelectorAll(".directors__back");
+
