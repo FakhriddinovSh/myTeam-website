ad33ebb1761feb054cf0f8ed10f2b45d56726f2b
FakhriddinovSh <shaxobiddinfaxriddinov@yandex.com>
2022-07-06 21:13:20 +0500

    Fully finished

--- js/main.js
+++
@@ -1,4 +0,0 @@
-let elBtn = document.querySelectorAll(".directors__icon");
-let elFront = document.querySelectorAll(".directors__content__box")
-let elBack = document.querySelectorAll(".directors__back");
-
